633c2c7031ddef4fd351c1470edd32b7937d6f73
Aleksandrs Domkov <adomkov@gmail.com>
2016-12-01 20:08:46 +0000

    Update RemoveVowels.py

--- RemoveVowels.py
+++ RemoveVowels.py
@@ -1,14 +1,14 @@
-string = input("string: ") # type string
-vowels = ("a","e","i","o","u","A","E","I","O","U") #tuple of vowels
+string = input("string: ")
+vowels = ("a","e","i","o","u","A","E","I","O","U")
 
 def Remove(string):
-    for element in string: # start loop, checking  letters in string
-        if element in vowels: # check if letter is in vowels
-            string = string.replace(element,"") # replace first occurance
-            Remove(string) # repete unitl no occurances
+    for element in string:
+        if element in vowels:
+            string = string.replace(element,"")
+            Remove(string)
     else:
-        return string # return string without vowels
+        return string
 
-print(Remove(string)) # inicialize function
+print(Remove(string))
 
             

da1c87a891c9cb5b42c5b6cc97beadd1c5e37a25
Aleksandrs Domkov <adomkov@gmail.com>
2016-12-01 19:53:44 +0000

    Update TrailingZeros.py

--- TrailingZeros.py
+++ TrailingZeros.py
@@ -1,12 +1,7 @@
 def calc(num):
-    mn = 1                          #1
-    answer = 0                      #1
-    while num >= mn:                #n
-        mn = mn*5                   #n
-        answer = answer + num//mn   #n
-    return answer                   #1
-
-#1 + 1 + n + n + n + 1
-#3 +3n /3
-#1 + n
-#O(n)
+    mn = 1                          
+    answer = 0                      
+    while num >= mn:                
+        mn = mn*5                   
+        answer = answer + num//mn   
+    return answer                   

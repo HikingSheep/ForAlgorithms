8df312453beeb62d05c3ffd736963e3e40f8a4bd
Aleksandrs Domkov <adomkov@gmail.com>
2016-12-01 20:05:22 +0000

    Update FindTheHighestSquare.py

--- FindTheHighestSquare.py
+++ FindTheHighestSquare.py
@@ -13,18 +13,3 @@ def calc(p):
 
     else:
         print(p,'is not a positive integral')
-
-
-#PERFECT_SQUARE(P)
-        #integral <- 0
-        #if(P is greater or equal 0) 
-            #while integral * integral < P
-                #integral <- integral + 1
-            #if(integral * integral != P)
-                #x <- sqrt(P)
-                #x <- floor(x**2)
-                #return (output( x is the highest square, input: P))
-            #else
-                #return (output( P  is a perfect square integral))
-        #else
-            #output(P is not a positive integral)
